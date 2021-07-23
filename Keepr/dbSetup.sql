deb4cf42579e77f47acfb8c79d8cbeb545661efb
Isaacgonzalez03 <Isaac.albert.g@gmail.com>
2021-07-23 14:31:21 -0600

    model setup table setup

--- Keepr/dbSetup.sql
+++ Keepr/dbSetup.sql
@@ -6,3 +6,30 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
+CREATE TABLE IF NOT EXISTS keeps(
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY COMMENT 'primary key',
+  creatorId VARCHAR(255) NOT NULL COMMENT 'FK: user account',
+  name VARCHAR(255) NOT NULL COMMENT 'user name',
+  description VARCHAR(255) NOT NULL COMMENT 'keep description',
+  img VARCHAR(255) NOT NULL COMMENT 'img url',
+  views INT NOT NULL DEFAULT 0 COMMENT 'view count',
+  shares INT NOT NULL DEFAULT 0 COMMENT 'share count',
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
+CREATE TABLE IF NOT EXISTS vaults(
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY COMMENT 'primary key',
+  creatorId VARCHAR(255) NOT NULL COMMENT 'FK: user account',
+  name VARCHAR(255) NOT NULL COMMENT 'user name',
+  description VARCHAR(255) NOT NULL COMMENT 'vault description',
+  isPrivate TINYINT NOT NULL COMMENT 'private marker',
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
+CREATE TABLE IF NOT EXISTS vault_keeps(
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY COMMENT 'primary key',
+  creatorId VARCHAR(255) NOT NULL COMMENT 'FK: user account',
+  vaultId INT NOT NULL COMMENT 'FK: vault id',
+  keepId INT NOT NULL COMMENT ' FK: keep id',
+  FOREIGN KEY (creatorId) REFERENCES accounts(id),
+  FOREIGN KEY (vaultId) REFERENCES vaults(id) ON DELETE CASCADE,
+  FOREIGN KEY (keepId) REFERENCES keeps(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
